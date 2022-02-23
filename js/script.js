2fa74f4af8a4f9f4125599d0e34fbef961a25770
MiltonMalagon <miltonmalagon1985@gmail.com>
2022-02-23 14:19:12 -0500

    Added "selectOtherJob" function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+// The "Name" field
 function focusNameField() {
     document.getElementById("name").focus();
 }
-
 focusNameField();
+
+// The "Job Role" section
+function selectOtherJob() {
+    const select = document.querySelector("#title");
+    const input = document.querySelector("#other-job-role");
+
+    input.style.display = "none"
+
+    select.addEventListener("change", (e) => {
+        if (e.target.value === "other") {
+            input.style.display = "block"
+        } else {
+            input.style.display = "none"
+        }
+    });
+}
+selectOtherJob();
