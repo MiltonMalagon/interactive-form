d0a6163aeb14274825f077bd35bd9d32b30b4960
MiltonMalagon <miltonmalagon1985@gmail.com>
2022-03-01 15:09:43 -0500

    Added "selectTShirtColor" function

--- js/script.js
+++ js/script.js
@@ -6,17 +6,55 @@ focusNameField();
 
 // The "Job Role" section
 function selectOtherJob() {
-    const select = document.querySelector("#title");
-    const input = document.querySelector("#other-job-role");
+    const jobMenu = document.querySelector("#title");
+    const jobInput = document.querySelector("#other-job-role");
 
-    input.style.display = "none";
-
-    select.addEventListener("change", (e) => {
+    jobInput.style.display = "none";
+    jobMenu.addEventListener("change", (e) => {
         if (e.target.value === "other") {
-            input.style.display = "block"
+            jobInput.style.display = "block"
         } else {
-            input.style.display = "none"
+            jobInput.style.display = "none"
         }
     });
 }
 selectOtherJob();
+
+// The "T-Shirt Info" section
+function selectTShirtColor() {
+    const designMenu = document.querySelector("#design");
+    const colorMenu = document.querySelector("#color");
+    const colorOptions = document.querySelectorAll("option[data-theme]");
+
+    colorMenu.disabled = true;
+    
+    designMenu.addEventListener("change", (e) => {
+        colorMenu.disabled = false;
+
+        let value = e.target.value;
+        
+        if (value === "js puns") {
+            for (let i = 0; i < colorOptions.length; i++) {
+                let dataset = colorOptions[i].dataset.theme;
+                
+                if (dataset !== value) {
+                    colorOptions[i].style.display = "none";
+                } else {
+                    colorOptions[i].style.display = "block";
+                }
+            }
+        }
+        if (value === "heart js") {
+            for (let i = 0; i < colorOptions.length; i++) {
+                let dataset = colorOptions[i].dataset.theme;
+                
+                if (dataset !== value) {
+                    colorOptions[i].style.display = "none";
+                } else {
+                    colorOptions[i].style.display = "block";
+                }
+            }
+        }
+    });
+}
+selectTShirtColor();
